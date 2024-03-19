01ecac87537a1939a099f061dd3efa48c3088b22
Pierre Brito <carlospierre07@gmail.com>
2024-03-18 21:54:16 -0300

    Configuração de CORS.

--- core/settings.py
+++ core/settings.py
@@ -40,17 +40,20 @@ INSTALLED_APPS = [
 
 
     #Rest Framework
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -105,6 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+#CORS CONFIG
+CORS_ALLOWED_ALL_ORIGINS = True
 
 
 # Internationalization
