b9bb5f92e1bf442e62f5ab24291d1bd7a9657e02
Pierre Brito <carlospierre07@gmail.com>
2024-03-21 21:19:40 -0300

    Rotas de groups

--- companies/urls.py
+++ companies/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from companies.views.employees import Employees, EmployeeDetail
 from companies.views.permissions import PermissionDetail
+from companies.views.groups import Groups, GroupDetail
 
 urlpatterns = [
     #Employees Endpoints
     path('employees', Employees.as_view()),
     path('employees/<int:employee_id>', EmployeeDetail.as_view()),
-    path('permissions/', PermissionDetail.as_view())
+    path('permissions/', PermissionDetail.as_view()),
+    path('groups/', Groups.as_view()),
+    path('groups/<int:group_id>', GroupDetail.as_view())
 ]
